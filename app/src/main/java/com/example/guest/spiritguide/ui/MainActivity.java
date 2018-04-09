109268bf771bb68a61c23acb374043f8811a786c
Kate Trahan <kathrynceciliatrahan@gmail.com>
2018-04-09 10:44:08 -0700

    add main activity and MA xml

--- app/src/main/java/com/example/guest/spiritguide/ui/MainActivity.java
+++ app/src/main/java/com/example/guest/spiritguide/ui/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.guest.spiritguide;
+package com.example.guest.spiritguide.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.guest.spiritguide.R;
 
 public class MainActivity extends AppCompatActivity {
 

6af5e0db9371c7cf537702342499c07f0cdc14d2
Kate Trahan <kathrynceciliatrahan@gmail.com>
2018-04-09 11:29:02 -0700

    add intent for getResponseButton

--- app/src/main/java/com/example/guest/spiritguide/ui/MainActivity.java
+++ app/src/main/java/com/example/guest/spiritguide/ui/MainActivity.java
@@ -1,15 +1,45 @@
 package com.example.guest.spiritguide.ui;
 
+import android.content.Intent;
+import android.support.annotation.MainThread;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 import com.example.guest.spiritguide.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    @BindView(R.id.getResponseButton) Button mGetResponseButton;
+    @BindView(R.id.userQuestionEditText) EditText mUserQuestionEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mGetResponseButton.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v){
+        if (v == mGetResponseButton) {
+            String question = mUserQuestionEditText.getText().toString();
+
+
+            if (question.equals("")) {
+                Toast.makeText(MainActivity.this, "Please type a question", Toast.LENGTH_LONG).show();
+            } else {
+                Intent intent = new Intent(MainActivity.this, SolutionsActivity.class);
+                startActivity(intent);
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/guest/spiritguide/ui/SolutionsActivity.java
@@ -0,0 +1,15 @@
+package com.example.guest.spiritguide.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.guest.spiritguide.R;
+
+public class SolutionsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_solutions);
+    }
+}
